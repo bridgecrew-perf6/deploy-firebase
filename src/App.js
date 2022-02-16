d817ee420e94a6ee8ef5fb8476ca4747ca6a952d
pablo179 <cm.pablo.prog@gmail.com>
2022-02-15 18:49:44 -0600

    feature 1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        Este cambio deberia estar bien
+        feature change 1
       </header>
     </div>
   );
